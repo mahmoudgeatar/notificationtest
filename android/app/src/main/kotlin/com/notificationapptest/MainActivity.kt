f772f909de204e417e66386b51c47ea6ee079be1
unknown <zidane_hoda2200@yahoo.com>
2021-05-20 13:59:31 +0200

    first

---
+++ android/app/src/main/kotlin/com/notificationapptest/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.notificationapptest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/sound.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Secound extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Secound"),
+      ),
+    );
+  }
+}
